21a13504f636261625b58a7a3d64029ba818809d
Mehmet Gezer <mehmetica74@gmail.com>
2022-12-20 11:48:00 +0300

    Librarian Test. Find.By.class/id

---
+++ src/test/java/com/cydeo/Day2Task4Librarian.java
@@ -0,0 +1,22 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day2Task4Librarian {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://library2.cybertekschool.com/login.html");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("inputEmail")).sendKeys("incorrectemail@gmail.com");
+
+        driver.findElement(By.id("inputPassword")).sendKeys("incorrectPassword");
+
+
+
+
+    }
+}
