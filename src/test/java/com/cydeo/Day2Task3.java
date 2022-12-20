063e242234a3f80f1fcb7791dca413b59c9567d6
Mehmet Gezer <mehmetica74@gmail.com>
2022-12-20 11:13:11 +0300

    enter a bastırma

--- src/test/java/com/cydeo/Day2Task3.java
+++ src/test/java/com/cydeo/Day2Task3.java
@@ -2,7 +2,9 @@ package com.cydeo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Day2Task3 {
@@ -11,6 +13,8 @@ public class Day2Task3 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
-       driver.findElement(By.name("q")).sendKeys("apple");
+
+        WebElement googleBox =driver.findElement(By.name("q"));
+        googleBox.sendKeys("apple" + Keys.ENTER); // enter a basma
     }
 }
